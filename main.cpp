d0019a1db319ddaf4fd7e672100127c189be8d30
alittlemc0 <alittlemc0@gmail.com>
2019-01-05 16:46:35 +0800

    提交测试1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 #include<algorithm>
 #include<cstdlib>
+
+template<class A>
+inline size_t Tsize(A &a){return (sizeof(a)/sizeof(a[0])); }
+
+template<class A>
+size_t fun(A *a1,A x)
+{
+    int len=Tsize(a1);
+    for(int s=0,e=len-1;s<len/2+len%2==0?0?1;s++,e--)
+    {
+        if(a1[s]>x)
+            return s;
+        if(a1[e]<x)
+            return e;
+    }
+}
 template<class A>
 A *fun(A *arr,A n,size_t m)
 {
