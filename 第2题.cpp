c84c7ea6074640a5b2da0ba5fc09b0bddc393f7b
alittlemc <43811017+alittlemc@users.noreply.github.com>
2019-01-05 23:56:34 +0800

    Create 第2题.cpp
    求分段函数

---
+++ 第2题.cpp
@@ -0,0 +1,20 @@
+/*y=x*x-1     (x<0)
+  y=x*x       (0≤x<1)
+  y=x*x+1     (x≥1)*/
+
+#include<iostream>
+
+template<class A>
+A fun(A x)
+{
+	return x*x+(x<0?-1:x>=1?1:0);
+}
+
+
+int main()
+{
+	double x;
+	std::cin>>x;
+	std::cout<<fun(x)<<std::endl;
+	return 0;
+}
